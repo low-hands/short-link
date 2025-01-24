62d2a535cf3b62c47d4a4e2e37b58bc01403470e
low-hands <GUOR0401@outlook.com>
2025-01-24 12:44:39 -0800

    start-application

---
+++ admin/src/main/java/org/shortlink/admin/ShortLinkAdmin.java
@@ -0,0 +1,11 @@
+package org.shortlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortLinkAdmin {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdmin.class, args);
+    }
+}
